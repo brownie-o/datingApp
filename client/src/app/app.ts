f2d66f84afb4f4ff45269c43bd7d8b59e98529b0
160795-yu <fjuais08@fju.edu.tw>
2026-01-08 09:15:22 +0800

    [feat] section 3 - install angular, tailwind, daisy UI

---
+++ client/src/app/app.ts
@@ -0,0 +1,43 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+
+// The @Component decorator transforms the App class into an Angular component.
+@Component({
+  selector: 'app-root', // specifies what element selector in index.html. e.g. <app-root> 
+  imports: [], // specifies what to import
+  templateUrl: './app.html', // specifies what template to use
+  styleUrl: './app.css' // specifies which styles to apply
+})
+
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+  protected members = signal<any>([])
+
+
+  // should reference using this. for the property in class
+
+  //if using subscribe, will need to unsubscribe to avoid memory leaks
+  // ngOnInit(): void {
+  //   this.http.get("https://localhost:5001/api/members").subscribe({
+  //     next: response => this.members.set(response),
+  //     error: error => console.log(error),
+  //     complete: () => console.log("Completed the http request")
+  //   });
+  // }
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers(){
+    try {
+      return lastValueFrom(this.http.get("https://localhost:5001/api/members"))
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+}
