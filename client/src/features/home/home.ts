dfdbec654bab1af79552509171854aae5a5e7b80
160795-yu <160795@mail.fju.edu.tw>
2026-04-09 15:27:04 +0800

    [feat] updated home component, angular commit workflow

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -1,6 +1,7 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
 import { Register } from "../account/register/register";
 import { User } from '../../types/user';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,7 @@ export class Home {
   // how to pass data to child component
   @Input({required: true}) membersFromApp: User[] = [];
 
+  protected accountService = inject(AccountService)
   protected registerMode = signal(false)
 
   showRegister(value: boolean) {
